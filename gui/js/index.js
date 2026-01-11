b13e87ba4db816eac26203aa330145eadf7c359c
blackpencil <48145759+blackpencil2019@users.noreply.github.com>
2026-01-11 12:51:18 +0800

    Change the default tab to dashboard

--- gui/js/index.js
+++ gui/js/index.js
@@ -81,8 +81,8 @@ function Root() {
 
                 <Hamburger onClick={() => setMenu(!menu)} />
                 <Menu className={menu ? "" : "menuHidden"}>
-                    <li><NavLink onClick={() => setMenu(false)} exact to="/">{loc.titleWifi}</NavLink></li>
-                    <li><NavLink onClick={() => setMenu(false)} exact to="/dashboard">{loc.titleDash}</NavLink></li>
+                    <li><NavLink onClick={() => setMenu(false)} exact to="/">{loc.titleDash}</NavLink></li>
+                    <li><NavLink onClick={() => setMenu(false)} exact to="/wifi">{loc.titleWifi}</NavLink></li>
                     <li><NavLink onClick={() => setMenu(false)} exact to="/config">{loc.titleConf}</NavLink></li>
                     <li><NavLink onClick={() => setMenu(false)} exact to="/files">{loc.titleFile}</NavLink></li>
                     <li><NavLink onClick={() => setMenu(false)} exact to="/firmware">{loc.titleFw}</NavLink></li>
@@ -101,16 +101,16 @@ function Root() {
                             binSize={binSize}
                             requestUpdate={fetchData} />
                     </Route>
-                    <Route exact path="/dashboard">
-                        <DashboardPage API={url} 
-                            socket={socket}
-                            requestData={() => {return displayData;}} />
+                    <Route path="/wifi">
+                        <WifiPage API={url} />
                     </Route>
                     <Route exact path="/firmware">
                         <FirmwarePage API={url} />
                     </Route>
-                    <Route path="/">
-                        <WifiPage API={url} />
+                    <Route exact path="/">
+                        <DashboardPage API={url} 
+                            socket={socket}
+                            requestData={() => {return displayData;}} />
                     </Route>
                 </Switch>
             </Page>
